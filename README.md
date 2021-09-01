b6c287e74aa77fb695308c3b48882780463eb94f
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-01 11:47:40 +1000

    feat: push files

---
+++ README.md
@@ -0,0 +1,28 @@
+<div align="center">
+
+# eslint-config
+
+[![CD](https://github.com/AstrielDivision/eslint-config/actions/workflows/CD.yml/badge.svg?branch=main)](https://github.com/AstrielDivision/eslint-config/actions/workflows/CD.yml)
+
+![GitHub issues](https://img.shields.io/github/issues-raw/AstrielDivision/eslint-config)
+![GitHub pull requests](https://img.shields.io/github/issues-pr/AstrielDivision/eslint-config)
+
+</div>
+
+## Installation
+
+`yarn add @astriel-division/eslint-config`
+
+## Usage
+
+Add this to your .eslintrc
+
+```json
+{
+	"extends": "@astriel-division/eslint-config"
+}
+```
+
+## License
+
+**Licensed under the [MIT License](https://github.com/AstrielDivision/eslint-config/blob/main/LICENSE)**
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+export = {
+	env: {
+		es2021: true,
+		node: true,
+		commonjs: true
+	},
+	extends: [
+		'eslint:recommended',
+		'standard-with-typescript',
+		'plugin:@typescript-eslint/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		tsconfigRootDir: '.',
+		project: './tsconfig.json',
+		sourceType: 'script'
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/indent': ['error', 'tab'],
+		'@typescript-eslint/quotes': ['error', 'single'],
+		'@typescript-eslint/semi': ['error', 'never'],
+		'@typescript-eslint/strict-boolean-expressions': 'off',
+    	'@typescript-eslint/space-before-function-paren': 'off',
+    	'@typescript-eslint/ban-ts-comment': 'off',
+    	'@typescript-eslint/prefer-ts-expect-error': 'off',
+		'@typescript-eslint/naming-convention': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'no-fallthrough': 'off',
+		'block-scoped-var': 'error',
+		'brace-style': [2, '1tbs'],
+		'eol-last': [2, 'always'],
+		'no-new': 'off',
+		eqeqeq: 'error',
+		'linebreak-style': ['error', 'unix'],
+		'no-console': 'off',
+		'no-unused-vars': 'error',
+		'no-extra-parens': 'error',
+		'no-await-in-loop': 'error',
+		'no-tabs': 'off',
+		'no-mixed-spaces-and-tabs': 'error',
+		'no-void': 'off',
+		'prefer-const': 'off',
+		indent: 'off',
+		yoda: 'error',
+	}
+}
